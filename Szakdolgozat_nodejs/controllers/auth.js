1dfb8a4b52f0b34680e4ed482c4dbebc5b623142
bodogadrian <43845751+bodogadrian@users.noreply.github.com>
2022-04-18 16:29:42 +0200

    Update auth.js

--- Szakdolgozat_nodejs/controllers/auth.js
+++ Szakdolgozat_nodejs/controllers/auth.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Sopron97",
+    password: "password",
     database: "db"
 });
 
